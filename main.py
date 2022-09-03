6de95fe3c015f9353c2ab09f650f4e618190d1e6
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 23:41:56 -0600

    add solution

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Write your code below. See the Instructions in the tab to the right
+books = int(input("How many books do you want to buy? "))
+money = int(input("How much money do you have? "))
+
+cost = books * 20
+
+if money >= cost:
+  print("You have enough money, go for it!")
+else:
+  difference = cost - money
+  print("You need $" + str(difference) + " more to buy that number of books")
