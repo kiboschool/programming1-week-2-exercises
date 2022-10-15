23a6503535a0c6737cf3e35a2dc9fbaa2727b220
Rob Cobb <rwcobbjr@gmail.com>
2022-10-15 18:10:24 +0000

    fix module cleanup

--- test.py
+++ test.py
@@ -8,28 +8,37 @@ class Test(TestCase):
     @patch('builtins.input', side_effect=["1", "21"])
     def test_enough_for_one(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("You have enough money, go for it!")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("You have enough money, go for it!")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["3", "61"])
     def test_enough_for_three(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("You have enough money, go for it!")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("You have enough money, go for it!")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["1", "19"])
     def test_short_for_one(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("You need $1 more to buy that number of books")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("You need $1 more to buy that number of books")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["3", "15"])
     def test_short_for_three(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("You need $45 more to buy that number of books")
+        try:
+            mock_print.assert_called_with("You need $45 more to buy that number of books")
+        finally:
+            sys.modules.pop('main')
 
 if __name__ == '__main__':
     unittest.main()
