501af1939e52ba3e19e8d1725c4e7ad22a4cf3d8
Rob Cobb <rwcobbjr@gmail.com>
2022-10-15 17:55:25 +0000

    fix cleanup

--- test_main.py
+++ test_main.py
@@ -8,29 +8,37 @@ class Test(TestCase):
     @patch('builtins.input', side_effect=["99"])
     def test_high_score(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("You passed!")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("You passed!")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["65"])
     def test_fail(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("Try again")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("Try again")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["70"])
     def test_70(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("Try again")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("Try again")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.print')
     @patch('builtins.input', side_effect=["71"])
     def test_71(self, mock_input, mock_print):
         import main
-        mock_print.assert_called_with("You passed!")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("You passed!")
+        finally:
+            sys.modules.pop('main')
 
 if __name__ == '__main__':
     unittest.main()
