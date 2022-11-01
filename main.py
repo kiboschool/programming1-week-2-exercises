b000e25a8badeed040cb489803a0edd07c522524
Mohammed S. Hassan <m.hassan@ogr.iu.edu.tr>
2022-11-01 13:53:53 +0300

    Update main.py
    use constants conventions + modify if conditions ranges to handle negative readings

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 # Electricity Prices
-kwh_price_low = 5
-kwh_price_med  = 10
-kwh_price_high = 15
+PRICE_ELECT_LOW_KWH = 5
+PRICE_ELECT_MED_KWH = 10
+PRICE_ELECT_HIGH_KWH = 15
 
-low_kwh_limit = 100
-med_kwh_limit = 1000
+LIMIT_KWH_LOW = 100
+LIMIT_KWH_MED = 1000
 
 # Water Prices
-water_price_low = 50
-water_price_med = 60
-water_price_high = 70
+PRICE_WATER_LOW_LITERS = 50
+PRICE_WATER_MED_LITERS = 60
+PRICE_WATER_HIGH_LITERS = 70
 
-low_water_limit = 250
-med_water_limit = 1500
+LIMIT_METER_LOW = 250
+LIMIT_METER_MED = 1500
 
 # Implementation
-bill_type = input("Enter the type of bill you want to calculate: \n 1) Electricity\n 2) Water\n Your Choice: ")
+bill_type = input(
+    "Enter the type of bill you want to calculate: \n 1) Electricity\n 2) Water\n Your Choice: ")
 
 if not (bill_type.isnumeric() and (int(bill_type) == 1 or int(bill_type) == 2)):
     print(f"Error: enter a number to choose a bill type from the list")
     exit()
 
+# we are sure from previous steps that bill_type is a number
 bill_type = int(bill_type)
 if bill_type == 1:
     meter_reading = input("Enter your electric meter reading in kwH: ")
@@ -36,24 +38,24 @@ meter_reading = int(meter_reading)
 if bill_type == 1:
     print(f"Calculating Electricity bill...")
 
-    if meter_reading <= low_kwh_limit:
-        bill_amount = meter_reading * kwh_price_low
-    elif meter_reading > low_kwh_limit and meter_reading < med_kwh_limit:
-        bill_amount = meter_reading * kwh_price_med
+    if 0 < meter_reading <= LIMIT_KWH_LOW:
+        bill_amount = meter_reading * PRICE_ELECT_LOW_KWH
+    elif meter_reading > LIMIT_KWH_LOW and meter_reading <= LIMIT_KWH_MED:
+        bill_amount = meter_reading * PRICE_ELECT_MED_KWH
     else:
-        bill_amount = meter_reading * kwh_price_high
+        bill_amount = meter_reading * PRICE_ELECT_HIGH_KWH
 
     print(f"Your total Electricity bill is ${bill_amount}")
 
 elif bill_type == 2:
     print(f"Calculating Water bill...")
 
-    if meter_reading <= low_water_limit:
-        bill_amount = meter_reading * water_price_low
-    elif meter_reading > low_water_limit and meter_reading < med_water_limit:
-        bill_amount = meter_reading * water_price_med
-    else: 
-        bill_amount = meter_reading * water_price_high
+    if 0 < meter_reading <= LIMIT_METER_LOW:
+        bill_amount = meter_reading * PRICE_WATER_LOW_LITERS
+    elif meter_reading > LIMIT_METER_LOW and meter_reading <= LIMIT_METER_MED:
+        bill_amount = meter_reading * PRICE_WATER_MED_LITERS
+    else:
+        bill_amount = meter_reading * PRICE_WATER_HIGH_LITERS
 
     print(f"Your total Water bill is ${bill_amount}")
 
